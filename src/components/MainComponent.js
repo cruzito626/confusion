9f6ecf102c6cb12e52b67b4cbb92a4e886bdb5b4
Edson Alexis Cruz Carballo <jhunnior.93@gmail.com>
2020-10-19 21:06:21 -0400

    asignment week 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,6 +7,7 @@ import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import About from './AboutComponent';
 
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
@@ -58,6 +59,7 @@ class Main extends Component {
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
           <Route exact path='/menu/:dishId' component={DishWithId} />
           <Route exact path='/contactus' component={Contact} />
+          <Route exact path='/aboutus' component={() => <About leaders={this.state.leaders} />} />
           <Redirect to="/home" />
         </Switch>
         <Footer/>
